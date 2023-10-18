57a3d0dce4d0b6d148cb158d388a6cd1e2d6e78f
fxhxyz <fxhxyz@proton.me>
2023-10-18 12:16:24 +0300

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,63 @@
 import pkg from './package.json' assert { type: "json" };
 import coffee from 'rollup-plugin-coffee-script';
+import { uglify } from "rollup-plugin-uglify";
+import babel from '@rollup/plugin-babel';
 
 let banner = '/** \n* @F fpcoffe v' + pkg.version + '\n'
   + '* @F (c)' + new Date().getFullYear() + ' fxhxyz \n'
   + '* @F MIT License\n'
-  + '* @F https://github.com/fxhxyz4/fpcoffee\n*/\n\n'
+  + '* @F github.com/fxhxyz4/fpcoffee\n*/\n\n'
 
 let config = {};
 
 export default config = {
-  input: 'src/index.coffee',
-  output: {
-    file: 'dist/fpcoffee.js',
-    format: 'iife',
-    banner: banner
-  },
+  input: './src/export.coffee',
+  output: [
+    {
+      file: './dist/fpcoffee.js',
+      format: 'esm',
+      sourcemap: true,
+      banner: banner
+    },
+    {
+      file: './dist/fpcoffee.cjs',
+      format: 'cjs',
+      sourcemap: true,
+      banner: banner
+    },
+  ],
   plugins: [
     coffee(),
+    babel(
+      {
+        exclude: 'node_modules/**',
+        babelrc: false,
+      }
+    )
   ],
+}
+
+if (process.env.NODE_ENV === 'prod') {
+  config.output = [
+    {
+      file: './dist/fpcoffee.min.js',
+      format: 'es',
+      sourcemap: true,
+    },
+    {
+      file: './dist/fpcoffee.min.cjs',
+      format: 'cjs',
+      sourcemap: true,
+    },
+  ],
+
+  config.plugins.push(
+    uglify({
+      compress: {
+        pure_getters: true,
+        unsafe: true,
+        unsafe_comps: true,
+      }
+    })
+  );
 }
